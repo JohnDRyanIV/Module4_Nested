7b5dd5121df4a49e1ebf068781a83067b6bdfc23
Diamondfiend <jdryan30@yahoo.com>
2020-02-12 19:44:43 -0600

    Implemented second set of tests, all fail

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -9,6 +9,14 @@ class MyTestCase(unittest.TestCase):
         assert topic2.calculate_price(9, 2, 20) == 11.89
         assert topic2.calculate_price(8, 5, 15) == 8.65
         assert topic2.calculate_price(9, 0, 0) == 15.49
+    def test_between_10_and_under_30(self):
+        assert topic2.calculate_price(15, 5, 20) == 14.43
+        assert topic2.calculate_price(25, 0, 15) == 30.48
+        assert topic2.calculate_price(12, 10, 20) == 7.65
+        assert topic2.calculate_price(29.99, 0, 0) == 39.74
+        assert topic2.calculate_price(20, 5, 15) == 21.47
+        assert topic2.calculate_price(18.76, 10, 10) == 14.31
+
 
 
 if __name__ == '__main__':
