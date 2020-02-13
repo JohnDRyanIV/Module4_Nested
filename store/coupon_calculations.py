4521f0813a85c1cbd7ff1e3b522c5465c9e92dd7
Diamondfiend <jdryan30@yahoo.com>
2020-02-12 19:34:44 -0600

    Partially implemented calculate_price, imported decimal to fix floating point math errors

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,6 +1,29 @@
+from decimal import *
+
+
 def calculate_price(price, cash_coupon, percentage_coupon):
-    pass
+    getcontext().prec = 20
+    total = price - cash_coupon  # Applying cash coupon discount
+    total = total - (total * Decimal((percentage_coupon / 100)))  # Applying percentage discount
+    tax = Decimal(total) * Decimal(.06)  # Calculating tax
+    # Adding shipping cost
+    if total < 10:
+        total = Decimal(total) + Decimal(5.95)
+
+    # Setting precision so that total is returned with proper number of decimal places
+    precision = 2
+    i = 1
+    while i < total:  # While loop determines precision necessary for proper decimal places
+        i = i * 10
+        precision = precision + 1
+
+    getcontext().prec = precision
+    total = Decimal(total) + Decimal(tax)
+    # Converting to float so other programs don't have to import decimal class
+    total = float(total)
+
+    return total
+
 
 if __name__ == '__main__':
     pass
-
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -2,13 +2,12 @@ import unittest
 from store import coupon_calculations as topic2
 
 
-
 class MyTestCase(unittest.TestCase):
     def test_under_10(self):
         assert topic2.calculate_price(5, 5, 0) == 5.95
-        assert topic2.calculate_price(7, 5, 20) == 7.646
-        assert topic2.calculate_price(9, 2, 20) == 11.886
-        assert topic2.calculate_price(8, 5, 15) == 8.653
+        assert topic2.calculate_price(7, 5, 20) == 7.65
+        assert topic2.calculate_price(9, 2, 20) == 11.89
+        assert topic2.calculate_price(8, 5, 15) == 8.65
         assert topic2.calculate_price(9, 0, 0) == 15.49
 
 
